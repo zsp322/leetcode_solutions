2dd5235c66e176d726bf4f251da0cd9f50fa949b
BerBerY <37963125+BerBerY@users.noreply.github.com>
2020-01-19 16:50:51 -0500

    Update 235.lowest_common_ancestor.java
    之前写的有点问题

--- Java/235.lowest_common_ancestor.java
+++ Java/235.lowest_common_ancestor.java
@@ -1,8 +1,30 @@
+// iterative
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return root;
-        if (p.val < root.val && q.val < root.val) return root;
-        if (q.val > root.val && p.val > root.val) return root;
-        return root;
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+        while(root != null){
+            if(p.val<root.val && q.val < root.val){
+                root = root.left;
+            }else if(p.val>root.val && q.val>root.val){
+                root = root.right;
+            }else{
+                return root;
+            }
+        }
+        return null;
+    }
+}
+// recursive
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+         if(p.val > root.val && q.val > root.val){
+            return lowestCommonAncestor(root.right, p, q);
+         }else if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left, p, q);
+         }else{
+             return root;
+         }    
     }
 }
